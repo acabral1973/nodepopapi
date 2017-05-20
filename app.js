e8cc81f1fa53d96019dcedc39b7e73f43995818f
Alejandro Cabral <cabralejandro@gmail.com>
2017-05-20 18:35:15 +0200

    agrego cluster

--- app.js
+++ app.js
@@ -33,20 +33,31 @@ app.use('/apiv0/initialize-db', require('./routes/initialize-db'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found leñe');
+  var err = new Error('Page not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
+
+  res.status(err.status || 500);
+
+  if (isAPI(req)) {
+    res.json({success: false, error: err.message});
+    return;
+  }
+
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
   res.render('error');
 });
+
+function isAPI(req){
+  return req.originalUrl.indexOf('/apiv') === 0;
+}
 
 module.exports = app;
